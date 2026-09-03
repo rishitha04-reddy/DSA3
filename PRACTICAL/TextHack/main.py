3b73446a0ab42160a5ad4d293317c89174202d07
rishitha04-reddy <sheririshithareddy@klh.edu.in>
2026-09-03 09:28:26 +0530

    Create main.py

---
+++ PRACTICAL/TextHack/main.py
@@ -0,0 +1,17 @@
+# Read the text file
+
+with open("sample.txt", "r") as file:
+    text = file.read()
+
+# Display the file content
+print("-----File Content-----")
+print(text)
+
+# Ask the user for a search word
+word = input("\nEnter the word to search: ")
+
+# Search for the word
+if word.lower() in text.lower():
+    print("\n Pattern Found")
+else:
+    print("\n Pattern Not Found")
